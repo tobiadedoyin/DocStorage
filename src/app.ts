20c9042bf69cb1fe16411fd8dae9e85f001966e8
Tobi Oladele <oladeletobiadedoyin@gmail.com>
2023-08-28 22:04:19 +0100

    library clean up

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import morgan from "morgan";
-import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import userRouter from "./route/user.router";
 import cors from "cors";
@@ -32,7 +31,6 @@ async () => {
 //middleware
 app.use(morgan(":date => :url => :method ==> :response-time ms"));
 app.use(cookieParser());
-app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
